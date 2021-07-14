074cc322cfe95eba3845485f577b6711d8e84219
vikrambadhan <vikram.badhan@gmail.com>
2021-07-14 19:14:13 +0530

    adding view engine ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+//set up view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
 
 app.listen(port, function(err){
     if (err){
