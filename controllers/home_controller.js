c738e7f3e05d6d96ba6ee35e2cd7a604d3665c55
vikrambadhan <vikram.badhan@gmail.com>
2021-07-14 19:26:42 +0530

    creating a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res){
-  return res.end('<h1>Express is up for Codeial!</h1>')
+    
+  return res.render('home', {
+      title :"home"
+  });
 }
 
 // module.exports.actionName = function(req, res){}
