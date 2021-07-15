d94223618e561c62f2422f33b8bcbe31afb3e271
vikrambadhan <vikram.badhan@gmail.com>
2021-07-16 02:12:13 +0530

    implementing Partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,5 @@
-module.exports.profile = function(req,
-  res){
-  res.end('<h1>User Profile</h1>');
-
+module.exports.profile = function(req, res){
+  return res.render('user_profile', {
+      title: 'User Profile'
+  })
 }
