14928a71445406bb1e59fbb68efbfb7761fc19a0
vikrambadhan <vikram.badhan@gmail.com>
2021-07-18 01:15:40 +0530

    Setting up user Sign up page to take in user credentials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function(req, res){
   return res.render('user_profile', {
       title: 'User Profile'
@@ -18,8 +21,24 @@ module.exports.signIn =  function(req,res){
 }
 
 // get the sign up data 
-module.exports.create = function(re,res){
-    //TODO later
+module.exports.create = function(req,res){
+  if(req.body.password != req.body.confirm_password){
+    return res.redirect('back');
+}
+console.log("Create function is called", req.body);
+
+User.findOne({email : req.body.email}, function(err, user){
+    if(err){ console.log('error in finding user in signing up'); return}
+
+    if (!user){
+        User.create(req.body, function(err, user){
+            if(err){ console.log('error in creating user while signing up'); return}
+            return res.redirect('/users/sign-in');
+        })
+    } else{
+        return res.redirect('back');
+    }
+});
 };
 
 // sign in and create session for user
