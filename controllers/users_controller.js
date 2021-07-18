3c65261092c05933833427d33403176dc946be8a
vikrambadhan <vikram.badhan@gmail.com>
2021-07-18 12:50:15 +0530

    Showing details of Signed-in user and adding a few more auth steps

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,10 +2,22 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req, res){
-  return res.render('user_profile', {
-      title: 'User Profile'
-  })
-}
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title : "User Profile",
+                    user : user
+                })
+            }else{
+                return res.redirect('/users/sign-in');
+            }
+        });
+ 
+    }else{
+        return res.redirect('/users/sign-in')
+    }
+ }
 //render the sign up page
 module.exports.signUp =  function(req,res){
   return res.render('user_sign_up',{
