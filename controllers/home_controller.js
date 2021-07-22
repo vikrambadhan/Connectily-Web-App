a90fb1b40fa2437cf7bb026d984575aacc6bb7ba
vikrambadhan <vikram.badhan@gmail.com>
2021-07-23 00:21:05 +0530

    Displaying posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,24 @@
-module.exports.home = function(req, res){
-  console.log(req.cookies);
-    res.cookie('user_id', 25);
+const Post = require('../models/post');
 
-    
-  return res.render('home', {
-      title :"home"
+
+module.exports.home = function(req, res){
+   // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts : posts
+    //     });
+   
+    // });
+
+     // populate user of each post
+     Post.find({}).populate('user').exec(function(err, posts){
+      return res.render('home',{
+          title : "Codeial | Home",
+          posts : posts
+      });
   });
 }
 
