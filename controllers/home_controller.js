397c207b959c088c4df193df0d1db3ce83dd5515
vikrambadhan <vikram.badhan@gmail.com>
2021-07-24 02:50:51 +0530

    Displaying comment and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,7 +14,15 @@ module.exports.home = function(req, res){
     // });
 
      // populate user of each post
-     Post.find({}).populate('user').exec(function(err, posts){
+     Post.find({})
+     .populate('user')
+     .populate({
+        path : 'comments',
+        populate : {
+            path : 'user'
+        }
+    })
+     .exec(function(err, posts){
       return res.render('home',{
           title : "Codeial | Home",
           posts : posts
