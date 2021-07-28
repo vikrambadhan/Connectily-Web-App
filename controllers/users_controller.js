dc94dc4ca8f5ecc22ad45be5e2d9d4f613285310
vikrambadhan <vikram.badhan@gmail.com>
2021-07-29 01:21:07 +0530

    Creating Flash messages in the layout

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -73,11 +73,13 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
+    req.flash('success', 'You have Logged out!');
     
     return res.redirect('/');
 }
