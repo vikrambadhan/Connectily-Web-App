4a11382296046fc546b28778c0d16b4ddca219f9
vikrambadhan <vikram.badhan@gmail.com>
2021-07-31 23:44:04 +0530

    Using JQUERY AJAX for the website

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,10 +4,20 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req,res){
   try{
-    await Post.create({
+    let post = await Post.create({
       content : req.body.content,
       user : req.user._id
     });
+
+    if(req.xhr){
+      return res.status(200).json({
+        data : {
+          post : post
+        },
+        message : "Post Created!"
+
+      });
+    }
 
       req.flash('success','Post published!');
       return res.redirect('back');
