d943be7d8c5668153b1e8cd71209c0f9974f920c
vikrambadhan <vikram.badhan@gmail.com>
2021-08-09 00:51:17 +0530

    Using google auth :: with a link

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -1,42 +1,44 @@
-const passport =  require('passport');
-const googlestrategy =  require('passport-google-oauth').OAuth2Strategy;
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto = require('crypto');
 const User = require('../models/user');
 
-//tell passport to use a new strategy for google login
+
+// tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
-    clientID : "1011483548736-fblb57j8rvftl4mhgj7i7v1m0jpkaqno.apps.googleusercontent.com",
-    clientSecret : "KQD9mXcBhjXc3kTgRfoOeQmR",
-    callbackURL : "http://localhost:8000/users/auth/google/callback",
+        clientID: '1011483548736-fblb57j8rvftl4mhgj7i7v1m0jpkaqno.apps.googleusercontent.com', // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
+        clientSecret: 'KQD9mXcBhjXc3kTgRfoOeQmR', // e.g. _ASDFA%KFJWIASDFASD#FAD-
+        callbackURL: "http://localhost:8000/users/auth/google/callback",
     },
 
     function(accessToken, refreshToken, profile, done){
-      User.findOne({email : profile.emails[0].value}).exec(function(err, user){
-        if(err){console.log('error in google strategy-passport',err); return;}
+        // find a user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+            if (err){console.log('error in google strategy-passport', err); return;}
+            console.log(accessToken, refreshToken);
+            console.log(profile);
 
-        console.log(profile);
+            if (user){
+                // if found, set this user as req.user
+                return done(null, user);
+            }else{
+                // if not found, create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function(err, user){
+                    if (err){console.log('error in creating user google strategy-passport', err); return;}
 
-        if(user){
-          //if found set this user as req.user
-          return done(null, user);
-        }else{
-          // if not found create the user and set it as req.user
-          User.create({
-            name : profile.displayName,
-            email : profile.emails[0].value,
-            password : crypto.randomBytes(20).toString('hex')
-          }, function(err, user){
-            if(err){ console.log('error in creating user google strategy-passport',err); return;}
+                    return done(null, user);
+                });
+            }
 
-            return done(null, user);
-          });
-
-        }
-
-      });
-
+        }); 
     }
+
 
 ));
 
-module.exports = passport ;
+
+module.exports = passport;
