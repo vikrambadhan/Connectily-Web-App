234255b677b43fbaf65b12e22cf198b2d691d8ab
vikrambadhan <vikram.badhan@gmail.com>
2021-08-13 02:13:00 +0530

    Setting up socket.io

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const sassMiddleware =require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
+// Setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
+
 
 // Note: you must place sass-middleware *before* `express.static` or else it will not work.
 app.use(sassMiddleware({
