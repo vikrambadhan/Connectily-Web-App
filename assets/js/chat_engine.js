cb99b5c7856bf32b326dbcb0761e5615231c8ef0
vikrambadhan <vikram.badhan@gmail.com>
2021-08-14 02:10:33 +0530

    creating and joining chat rooms

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -15,9 +15,20 @@ class ChatEngine{
 
 
   connectionHandler(){
-    console.log("called")
+      let self = this;
+
       this.socket.on('connect', function(){
           console.log('connection established using sockets...!');
+
+          self.socket.emit('join_room', {
+            user_email: self.userEmail,
+            chatroom: 'codeial'
+          });
+
+          self.socket.on('user_joined', function(data){
+            console.log('a user joined', data);
+          })
+
       });
   }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -5,5 +5,21 @@ module.exports.chatSockets = function(socketServer){
         io.sockets.on('connection', function(socket){
            console.log('new connection received', socket.id);
 
-     })
+           socket.on('disconnect', function(){
+            console.log('socket disconnected!');
+        });
+        
+        socket.on('join_room', function(data){
+            console.log('joining request rec.', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        })
+
+    });
+
 }
+
+
+
