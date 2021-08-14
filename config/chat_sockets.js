9ed41c7c716d778683e42ba4e79bcab013299ae4
vikrambadhan <vikram.badhan@gmail.com>
2021-08-14 11:36:19 +0530

    Sending and receiving messages in Chat rooms

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,13 +1,14 @@
-module.exports.chatSockets = function(socketServer){
-        let io = require('socket.io')(socketServer);
-      console.log("avout to connect");
-      
-        io.sockets.on('connection', function(socket){
-           console.log('new connection received', socket.id);
 
-           socket.on('disconnect', function(){
+module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received', socket.id);
+
+        socket.on('disconnect', function(){
             console.log('socket disconnected!');
         });
+
         
         socket.on('join_room', function(data){
             console.log('joining request rec.', data);
@@ -15,11 +16,13 @@ module.exports.chatSockets = function(socketServer){
             socket.join(data.chatroom);
 
             io.in(data.chatroom).emit('user_joined', data);
-        })
+        });
+
+        // CHANGE :: detect send_message and broadcast to everyone in the room
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
 
     });
 
 }
-
-
-
